abc0f7e0a203fc2f9288e3c554e4667d5e05c556
avoineu <21160@ecam.be>
2025-10-07 11:37:12 +0200

    Implement MongoDB integration and update Kubernetes configurations

--- app/app.py
+++ app/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, jsonify
 import socket
+import os
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# Variables d’environnement (configurées dans le Deployment)
+MONGO_USER = os.getenv("MONGO_USER", "store_admin")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "store_pass")
+MONGO_HOST = os.getenv("MONGO_HOST", "mongodb")
+MONGO_PORT = int(os.getenv("MONGO_PORT", "27017"))
+
+# Connexion à MongoDB
+client = MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/")
+db = client["store_db"]
+products_collection = db["products"]
 
 # Liste statique de produits
 products = [
@@ -17,6 +30,7 @@ def home():
 
 @app.route('/api/products')
 def get_products():
+    products = list(products_collection.find({}, {"_id": 0}))
     return jsonify(products)
 
 if __name__ == '__main__':
