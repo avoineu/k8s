76eb65f5175faec7a7ddd3adadd398dbde841b2c
avoineu <21160@ecam.be>
2025-10-08 12:56:37 +0200

    Refactor MongoDB integration and update Kubernetes configurations for replica set support

--- app/app.py
+++ app/app.py
@@ -5,28 +5,17 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Variables d’environnement (configurées dans le Deployment)
-MONGO_USER = os.getenv("MONGO_USER", "store_admin")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "store_pass")
-MONGO_HOST = os.getenv("MONGO_HOST", "mongodb")
-MONGO_PORT = int(os.getenv("MONGO_PORT", "27017"))
-
-# Connexion à MongoDB
-client = MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/")
-db = client["store_db"]
+# URI du replica set
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["store"]
 products_collection = db["products"]
-
-# Liste statique de produits
-products = [
-    {"id": 1, "name": "T-shirt Kubernetes", "price": 20},
-    {"id": 2, "name": "Mug Docker", "price": 10},
-    {"id": 3, "name": "Casquette Cloud", "price": 15}
-]
 
 @app.route('/')
 def home():
     hostname = socket.gethostname()
-    return render_template('index.html', pod_name=hostname)
+    products = list(products_collection.find({}, {"_id": 0}))
+    return render_template('index.html', pod_name=hostname, products=products)
 
 @app.route('/api/products')
 def get_products():
