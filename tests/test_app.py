175e7a1025e02958782a4c052747ee8cc1a41cd6
avoineu <21160@ecam.be>
2025-10-12 22:50:58 +0200

    refactor: reorganize import statements

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
-import pytest
-from unittest.mock import patch
-from app import app  # suffisant pour accéder à Flask et aux collections
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
+from app import app 
 
 @pytest.fixture
 def client():
